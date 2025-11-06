551bc839dfce0cfee3798bff2e53d99ea786ebe4
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-06 15:39:05 +0000

    commit

---
+++ matches.js
@@ -0,0 +1,5 @@
+/*
+matches identifies that if the given element has the given query selector or not
+closest determines which of the element's ancestors have the give query  selector 
+contains is used to identify that if the which child element has the following selectors
+*/
