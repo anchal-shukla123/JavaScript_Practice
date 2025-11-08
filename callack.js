f3579e71b8b12c8e84528f4a7f6ee6c17db804a6
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-08 16:51:54 +0000

    commit

--- callack.js
+++ callack.js
@@ -1,14 +1,26 @@
 //Asynchronous Programming
 
-console.log("start")
-setTimeout(
-    function fun() {
-        console.log("Now executing timeout function");
-    }, 3000
-)
-console.log("End")
+// console.log("start")
+// setTimeout(
+//     function fun() {
+//         console.log("Now executing timeout function");
+//     }, 3000
+// )
+// console.log("End")
 
 /*
 A callback is a function that is passed into another function as an argument and is executer 
 insite the outer function to complete its execution
 */
+
+function load(src, callback){
+    let script= document.createElement('script')
+    script.src= src
+    script.onload=()=>callback(src)
+    script.onerror=()=>callback("Error hae")
+    document.head.append(script);
+}
+function hello(msg){
+    console.log("hello from bhagwaan !"+ msg);
+}
+load("https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js", hello);
