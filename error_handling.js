69280b56608a756f76f356a4f7580c41d22e0321
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-18 16:24:17 +0000

    day7

---
+++ error_handling.js
@@ -0,0 +1,21 @@
+try{
+    console.log(arr);
+}
+catch(err){
+    console.log(err);
+}
+function fun(){
+    try{
+        setTimeout(()=>{
+            console.log("nums"  )//try block does not work in async functions
+        },2000)
+        return
+    }
+    catch(error){
+        console.log("Error mil gaye");
+    }
+    finally{
+        console.log("this is finally");
+    }
+}
+fun();
