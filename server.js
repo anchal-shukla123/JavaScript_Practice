072555549612ff09e410dac102a597fc53779f21
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-25 15:34:29 +0000

    day 9

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
  const http = require('http');
+ const fs=require('fs');
  const server = http.createServer((req, res)=> {
     console.log(req.url, req.method, req.headers);
     if(req.url==='/'){
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My Title</title></head>');
-    res.write('<body><h1>Hello</h1></body>');
+    res.write('<body>');
+    res.write('<h1>MY FORM</h1>');
+    res.write('<form action="/submit-url" method="POST">');
+    res.write('<input type="text" placeholder="Enter your name" name="username"><br/>');
+    res.write('<input type="radio" value="Male" id="male" name="gender">');
+    res.write('<label for="male">Male</label>')
+    res.write('<input type="radio" value="Female" id="female" name="gender">');
+    res.write('<label for="female">Female</label><br/>')
+    res.write('<button type="submit">Submit</button>')
+    res.write('</form>');
+    res.write('</body>');
     res.write('</html>');
     return res.end();
+    }
+    else if(req.url.toLowerCase()==="/submit-url" && req.method=="POST"){
+      fs.writeFileSync('user.txt', 'anchal shukla');
+      res.statusCode= 302;
+      res.setHeader('Location', '/');
     }
     else if(req.url==='/products'){
       res.setHeader('Content-Type','text/html');
@@ -18,7 +34,7 @@
     return res.end();
     }
     else{
-      res.setHeader('Content-Type','text/html');
+    res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>others</title></head>');
     res.write('<body><h1>Others</h1></body>');
