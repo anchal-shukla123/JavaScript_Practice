9b8594e86c50bdbab6383e47a33939f31ccf8ffe
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-27 17:07:31 +0000

    day 12

---
+++ project/app.js
@@ -0,0 +1,8 @@
+const http= require('http');
+const server= http.createServer((req, res)=>{
+
+});
+server.listen(4005, ()=>{
+    console.log('server started');
+    console.log('server started');
+})
