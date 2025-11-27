21997288314e1ffbb518da4c9545b0158d5a9bd8
anchal-shukla123 <anchalshukla423@gmail.com>
2025-11-27 16:34:38 +0000

    day 12

--- sum.js
+++ sum.js
@@ -3,12 +3,16 @@ const sumHandler= (req, res)=>{
    req.on('data', (chunk)=>{
      body.push(chunk);
    })
+   
    req.on('end', ()=>{
    const ans=Buffer.concat(body).toString();
    const params= new URLSearchParams(ans);
    const data=Object.fromEntries(params);
    const val=Number(data.num1)+Number(data.num2);
    console.log(val);
+   res.setHeader('Content-Type', 'text/html');
+   res.write(`<html><head><title>Calculator</title></head><body><h1>Your Sum is ${val} </h1></body></html>`);
+
    })
-}
+} 
 exports.sumHandler=sumHandler;
