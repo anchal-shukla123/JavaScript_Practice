f3f5109f46dbd8151c436ad68b1a9cc985448ac2
anchal-shukla123 <anchalshukla423@gmail.com>
2025-12-07 06:01:15 +0000

    git

---
+++ Node_practice/express.js
@@ -0,0 +1,16 @@
+/*const http= require('http');----> not required*/
+const express= require('express');
+const app=express();
+app.use('/tab',(req, res, next)=>{
+   console.log(req.url, req.method);
+   next();
+});
+app.use('/tab',(req, res, next)=>{
+    res.send('<p> Welcome to express tab</p>')
+});
+app.use('/second',(req, res, next)=>{
+    res.send('<p> Welcome to express second</p>')          //calling res.send() implicitly calls res.end()
+});
+app.listen(4009, ()=>{
+    console.log("server ban gaya!!!");
+})
